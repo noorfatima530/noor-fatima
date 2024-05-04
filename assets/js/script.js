cf36df7023e3a76cb0b440023d40d1c537bc9492
noorfatima530 <150837836+noorfatima530@users.noreply.github.com>
2024-05-04 15:52:50 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 
 
 /**
+ * form submitted button
+ */
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    var response_message = document.getElementById("response_message");
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => response_message.innerHTML = "Thank you! your form is submitted successfully.")
+      .catch(error => response_message.innerHTML = "Error!")
+  })
+
+/**
  * element toggle function
  */
 
